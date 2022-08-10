333844d5908e1aab32b21d3e9676c41444ca5a28
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-08-10 14:54:47 +0200

    Create 5-Strings.js
    Learning about strings and how to slice them up, lower/upper case

---
+++ Humble-Javascript-Bundle-Curriculum/The-Complete-Beginner-JavaScript-Course/5-Strings.js
@@ -0,0 +1,13 @@
+var characterName = "Eirik";
+characterName = "Zenva";
+var age = 26;
+
+let message = `Hi, my name is ${characterName} and my age is ${age}`;
+
+let length = characterName.length;
+let upperName = characterName.toUpperCase();
+characterName= characterName.toLowerCase();
+
+let zen = characterName.slice(1, 4);
+
+console.log(zen);
