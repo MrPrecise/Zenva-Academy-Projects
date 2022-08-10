8e5c430a912198abb88f6d6de426e63642f3f383
Eirik <MrPrecise@users.noreply.github.com>
2022-08-10 11:42:22 +0200

    Create 8-Operators.js
    Lessons about Arithmetic, Comparison and Logical operators

---
+++ Humble-Javascript-Bundle-Curriculum/Javascript-Fundementals/8-Operators.js
@@ -0,0 +1,14 @@
+//Assignment: =
+var age = 26;
+var firstName = "Eirik";
+var money = 50.05;
+//Arithmetic: + - * / %
+age = age + 1
+var newAge = age + 1
+age += 1;
+var fullName = firstName + " Thue";
+// Comparison: == != > >= < <=
+var canAfford = money >= 20;
+// Logical: || && !
+var cannotAfford = !canAfford;
+var canBuy = canAfford && age >= 18;
