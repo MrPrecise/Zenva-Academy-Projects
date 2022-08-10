3abd1330c77e0e560ca2731beb234300361722d2
Eirik <MrPrecise@users.noreply.github.com>
2022-08-10 11:34:40 +0200

    Lesson 6 and 7
    Followed along the lessons and trying out different types of variables

---
+++ Humble-Javascript-Bundle-Curriculum/Javascript-Fundementals/6-Running-JavaScript-Code.js
@@ -0,0 +1,6 @@
+var i = 5;
+console.log(i);
+
+// This is a comment
+/* This is a multi 
+line comment */
---
+++ Humble-Javascript-Bundle-Curriculum/Javascript-Fundementals/7-Variables.js
@@ -0,0 +1,12 @@
+var age = 27;
+var money = 50.05;
+age = 28;
+
+var isUserLoggedIn = true;
+
+var name = "Eirik";
+
+// name = 60;
+
+const title = "My Webpage"
+/* title   = "New title"      can't be changed */
