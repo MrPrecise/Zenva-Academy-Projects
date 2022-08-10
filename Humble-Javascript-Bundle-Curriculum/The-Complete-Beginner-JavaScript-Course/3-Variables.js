d018b57e9b3c356623c9b52ad6c96505dc058d81
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-08-10 14:33:03 +0200

    Created files following 3 and 4
    Created files following 3 and 4

---
+++ Humble-Javascript-Bundle-Curriculum/The-Complete-Beginner-JavaScript-Course/3-Variables.js
@@ -0,0 +1,9 @@
+var maxHealth = 100;
+var currentHealth = 50;
+ currentHealth = maxHealth;
+ //maxHealth = currentHealth;
+
+ var isGameOver = false;
+ isGameOver = true;
+
+ console.log(isGameOver);
