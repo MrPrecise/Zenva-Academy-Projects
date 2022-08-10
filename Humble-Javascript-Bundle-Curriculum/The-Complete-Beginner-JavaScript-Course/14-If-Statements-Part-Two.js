a8414da2ce9cb4f3ac40a36dfe1014514c185a26
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-08-10 16:24:06 +0200

    Update 14-If-Statements-Part-Two.js

--- Humble-Javascript-Bundle-Curriculum/The-Complete-Beginner-JavaScript-Course/14-If-Statements-Part-Two.js
+++ Humble-Javascript-Bundle-Curriculum/The-Complete-Beginner-JavaScript-Course/14-If-Statements-Part-Two.js
@@ -1,12 +1,27 @@
 var keyPressed = "l";
 var xPos = 0;
+let endPos = 5;
 
-if (keyPressed == "l"){
-    console.log("Move Left");
-    xPos -= 1;
-} else if  (keyPressed == "r") {
-    console.log("Move Right");
+
+if  (keyPressed == "r" && xPos < endPos) {
+        xPos += 1;
+}
+
+if (keyPressed == "r"){
+   if(xPos < endPos){
     xPos += 1;
+   }
+} else if  (keyPressed == "l") {
+    xPos -= 1;
 } else {
     xPos = 0;
 }
+
+// var someNum = 5;
+
+// if ( someNum > 4 ){
+//     xPos += 1;
+// }
+// if (someNum > 2){
+//     xPos -= 1;
+// }
