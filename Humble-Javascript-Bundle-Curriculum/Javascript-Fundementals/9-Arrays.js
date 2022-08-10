a2b44bf8ee33e188401f10efa835c3761bc248f0
Eirik <MrPrecise@users.noreply.github.com>
2022-08-10 12:18:07 +0200

    Create 9-Arrays.js
    Learning about how arrays work

---
+++ Humble-Javascript-Bundle-Curriculum/Javascript-Fundementals/9-Arrays.js
@@ -0,0 +1,10 @@
+var shoppingList = ["Butter", "Milk", "Eggs",];
+console.log(shoppingList);
+
+shoppingList = ["Eggs", "Butter", "Apples",];
+var eggs = shoppingList[0];
+shoppingList[1] = "Cheese";
+
+var listLenth = shoppingList.length;
+shoppingList.push("Butter");
+var butter = shoppingList.pop();
