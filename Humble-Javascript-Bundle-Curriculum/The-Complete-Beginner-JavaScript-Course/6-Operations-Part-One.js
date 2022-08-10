06e4bc1498ead75e146e23f0408599cd1ba54030
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-08-10 15:05:26 +0200

    Create 6-Operations-Part-One.js

---
+++ Humble-Javascript-Bundle-Curriculum/The-Complete-Beginner-JavaScript-Course/6-Operations-Part-One.js
@@ -0,0 +1,23 @@
+var health = 50;
+
+// + - * / % **
+
+health = health + 10; // 60
+health = health % 50; // 10
+// Check if it's even: health % 2 == 0
+health = health ** 2; // 100
+
+// +=, -=,  *=, /=
+
+health -= 20; // health = health - 20
+
+// ++, -- 
+health ++; // 81
+health --; // 80
+
+// health = (health * 2) + (health / 5)
+
+//Homework
+health = health + 1;
+health ++;
+health += 1;
