867993cf4798987b5a976c654966a5f988a0bdbd
Eirik <MrPrecise@users.noreply.github.com>
2022-08-15 19:14:00 +0200

    Lesson 9 Sprite Sheet
    Learned of how to add a sprite sheet and change between the sprites in the sprite sheet

--- Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/main.js
+++ Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/main.js
@@ -18,6 +18,7 @@ function init(){
 
 function preload(){
     this.load.image('button1', 'assets/images/ui/blue_button01.png');
+    this.load.spritesheet('items', 'assets/images/items.png', {frameWidth: 32, frameHeight: 32})
 }
 
 function create(){
@@ -25,6 +26,7 @@ function create(){
  button.setOrigin(0.5, 0.5);
 
  this.add.sprite(300, 100, 'button1');
+ this.add.image(300, 300, 'items', 1);
 }
 
 function update(){
