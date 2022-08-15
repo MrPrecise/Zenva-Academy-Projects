5f0d5a597ca53fd2b2a6b8e6e81a9004763220fa
Eirik <MrPrecise@users.noreply.github.com>
2022-08-15 19:08:50 +0200

    Lesson 7 Images
    Learned how to add sprites and images for our game development while also learning a bit about positioning and origins of an object

--- Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/main.js
+++ Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/main.js
@@ -3,17 +3,30 @@ var config ={
     width: 800,
     heigth: 600,
     scene: {
+        init: init,
         preload: preload,
         create: create,
+        update: update,
     },
 };
 
 var game = new Phaser.Game(config);
 
-function preload(){
+function init(){
 
 }
 
+function preload(){
+    this.load.image('button1', 'assets/images/ui/blue_button01.png');
+}
+
 function create(){
+ var button = this.add.image(100, 100, 'button1');
+ button.setOrigin(0.5, 0.5);
+
+ this.add.sprite(300, 100, 'button1');
+}
+
+function update(){
 
 }
