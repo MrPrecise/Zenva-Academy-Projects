332bf795dfd638404aa7a4198a71e4f283ff04eb
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-08-16 19:06:09 +0200

    Lesson 21 Game Scene
    Seperated old create() and update() into the GameScene.js

--- Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/scenes/BootScene.js
+++ Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/scenes/BootScene.js
@@ -8,47 +8,9 @@ class BootScene extends Phaser.Scene {
         this.load.spritesheet('items', 'assets/images/items.png', { frameWidth: 32, frameHeight: 32 });
         this.load.spritesheet('characters', 'assets/images/characters.png', { frameWidth: 32, frameHeight: 32 });
         this.load.audio('goldSound', ['assets/audio/Pickup.wav']);
-    }
-
-    create() {
-        var goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 0.2 });
-      
-        var button = this.add.image(100, 100, 'button1');
-        button.setOrigin(0.5,0.5);
-      
-        this.add.sprite(300,100, 'button1');
-      
-        this.chest = this.physics.add.image(300, 300, 'items', 0);
-      
-        this.wall = this.physics.add.image(500, 100, 'button1');
-        this.wall.setImmovable();
-      
-        this.player = this.physics.add.image(32, 32, 'characters', 0);
-        this.player.setScale(2);
-      
-        this.player.body.setCollideWorldBounds(true);
-        this.physics.add.collider(this.player, this.wall);
-        this.physics.add.overlap(this.player, this.chest, function(player, chest) {
-          goldPickupAudio.play(); 
-          chest.destroy();
-        });
-      
-        this.cursors = this.input.keyboard.createCursorKeys();
       }
-      
-      update() {
-        this.player.setVelocity(0);
-      
-        if(this.cursors.left.isDown) {
-          this.player.setVelocityX(-160);
-        } else if(this.cursors.right.isDown) {
-          this.player.setVelocityX(160);
-        }
-      
-        if(this.cursors.up.isDown) {
-          this.player.setVelocityY(-160);
-        } else if(this.cursors.down.isDown) {
-          this.player.setVelocityY(160);
-        }
+
+      create(){
+        this.scene.start('Game');
       }
     }
