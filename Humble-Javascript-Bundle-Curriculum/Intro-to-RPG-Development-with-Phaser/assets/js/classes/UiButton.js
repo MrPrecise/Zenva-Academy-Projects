aaf4a42196b787b913d898747be1761dae83b059
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-08-16 22:32:38 +0200

    Lesson 29 Implementing UI Button Class
    Added the new UiButton to the title screen with the class instead of the non modular version that is already on the title screen before this lesson

--- Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/classes/UiButton.js
+++ Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/classes/UiButton.js
@@ -1,6 +1,6 @@
-class UiButton extends Phaseer.GameObjects.Container{
-    constructor(screen, x, y, key, hoverKey, text, targetCallback){
-        super(screen, x, y);
+class UiButton extends Phaser.GameObjects.Container {
+    constructor(scene, x, y, key, hoverKey, text, targetCallback) {
+        super(scene, x, y);
         this.scene = scene;
         this.x = x;
         this.y = y;
@@ -12,34 +12,46 @@ class UiButton extends Phaseer.GameObjects.Container{
         this.createButton();
         this.scene.add.existing(this);
     }
-    createButton(){
-        this.button = this.add.image(
+
+    createButton() {
+        this.button = this.scene.add.image(
                 0,
                 0,
-                'button1');
+                'button1'
+            );
+
         this.button.setInteractive();
+
         this.button.setScale(1.4);
-        this.buttonText = this.add.text(
+
+        this.buttonText = this.scene.add.text(
                 0,
                 0,
                 this.text, {
                     fontSize: '30px',
                     fill: '#fff'
                 });
+
         Phaser.Display.Align.In.Center(
-            this.buttonText, 
-            this.button);
+                this.buttonText, 
+                this.button
+            );
+
             this.add(this.button);
             this.add(this.buttonText);
+
         this.button.on('pointerover', () => {
                 this.button.setTexture(this.hoverKey);
             });
+
         this.button.on('pointerout', () => {
                 this.button.setTexture(this.key);
             });
+
         this.button.on('pointerdown', () => {
                 this.targetCallback();
             });
     }
+    
 }
 
--- Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/scenes/TitleScene.js
+++ Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/scenes/TitleScene.js
@@ -11,28 +11,24 @@ class TitleScene extends Phaser.Scene{
                 fontSize: '64px',
                 fill: '#fff'
             });
+
         this.titleText.setOrigin(0.5)
-        this.button = this.add.image(
+
+        this.startGameButton = new UiButton(
+            this,
             this.scale.width / 2, 
-            this.scale.height * 0.65, 
-            'button1');
-        this.button.setInteractive();
-        this.buttonText = this.add.text(
-                0,
-                0,
-                'Start', {
-                    fontSize: '30px',
-                    fill: '#fff'
-                });
-        Phaser.Display.Align.In.Center(this.buttonText, this.button);
-        this.button.on('pointerover', () => {
-                this.button.setTexture('button2');
-            });
-        this.button.on('pointerout', () => {
-                this.button.setTexture('button1');
-            });
-        this.button.on('pointerdown', () => {
-                this.scene.start('Game')
-            });
+            this.scale.height * 0.65,
+            'button1',
+            'button2',
+            'Start',
+            this.startScene.bind(
+                this, 
+                'Game'
+            )
+        );
     }
+
+    startScene(targetScene) {
+        this.scene.start(targetScene);
+      }
 }
