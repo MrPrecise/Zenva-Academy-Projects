3b18d9868b9009af57ff30f69c41aa3d4b2379cf
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-08-16 23:16:46 +0200

    Lesson 33 UI Scene Elements
    Added functionality for the UiScene to have an event that takes in the amount of coins the user has collected and update the UI coins text

--- Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/scenes/GameScene.js
+++ Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/scenes/GameScene.js
@@ -2,9 +2,13 @@ class GameScene extends Phaser.Scene{
     constructor(){
         super('Game')
     }
+
+    init(){
+      this.scene.launch('Ui');
+    }
     
     create() {
-        var goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 0.2 });
+        this.goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 0.2 });
 
         this.chest = new Chest(this, 300, 300, 'items', 0);
       
@@ -14,16 +18,28 @@ class GameScene extends Phaser.Scene{
         this.player = new Player(this, 32, 32, 'characters', 6);
 
 
-        this.physics.add.collider(this.player, this.wall);
-        this.physics.add.overlap(this.player, this.chest, function(player, chest) {
-          goldPickupAudio.play(); 
-          chest.destroy();
-        });
+        this.physics.add.collider(
+            this.player, 
+            this.wall
+          );
+        this.physics.add.overlap(
+            this.player, 
+            this.chest,
+            this.collectChest,
+            null,
+            this 
+          );
       
         this.cursors = this.input.keyboard.createCursorKeys();
       }
       
-      update() {
-       this.player.update(this.cursors);
-      }
+    update() {
+        this.player.update(this.cursors);
+    }
+
+    collectChest(player, chest){
+      this.goldPickupAudio.play();
+      this.events. emit('updateScore', chest.coins)
+      chest.destroy();
+    }
 }
