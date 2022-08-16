b980680a5d9e9352baa9d16edaf64135148e3495
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-08-16 20:24:23 +0200

    Lesson 25 Title Scene
    Changed the BootScene to be TitleScene instead of GameScene, then added text and a interactive button to start the GameScene from the TitleScene

--- Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/scenes/TitleScene.js
+++ Humble-Javascript-Bundle-Curriculum/Intro-to-RPG-Development-with-Phaser/assets/js/scenes/TitleScene.js
@@ -3,11 +3,36 @@ class TitleScene extends Phaser.Scene{
         super('Title')
     }
 
-    preload(){
-
-    }
-
     create(){
-        
+        this.titleText = this.add.text(
+            this.scale.width / 2, 
+            this.scale.height / 2, 
+            'Precise RPG', {
+                fontSize: '64px',
+                fill: '#fff'
+            });
+        this.titleText.setOrigin(0.5)
+        this.button = this.add.image(
+            this.scale.width / 2, 
+            this.scale.height * 0.65, 
+            'button1');
+        this.button.setInteractive();
+        this.buttonText = this.add.text(
+                0,
+                0,
+                'Start', {
+                    fontSize: '30px',
+                    fill: '#fff'
+                });
+        Phaser.Display.Align.In.Center(this.buttonText, this.button);
+        this.button.on('pointerover', () => {
+                this.button.setTexture('button2');
+            });
+        this.button.on('pointerout', () => {
+                this.button.setTexture('button1');
+            });
+        this.button.on('pointerdown', () => {
+                this.scene.start('Game')
+            });
     }
 }
