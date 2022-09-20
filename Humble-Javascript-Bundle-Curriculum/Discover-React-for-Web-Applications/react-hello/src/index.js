74aa7fb3fbd19f474eba8ac066a55ece2c34d79e
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-09-20 16:58:48 +0200

    Lesson 8
    Props

--- Humble-Javascript-Bundle-Curriculum/Discover-React-for-Web-Applications/react-hello/src/index.js
+++ Humble-Javascript-Bundle-Curriculum/Discover-React-for-Web-Applications/react-hello/src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function HelloWorld(){
-  return(
-    <div>
-      <Hello/>
-    </div>
+function HelloWorld({name}) {
+  return (
+      <h1>Hello, {name}</h1>
   );
 }
 
-function Hello(){
-  var isHello = false;
-  return(
-   <span>
-    {isHello && "Hello"}
-    {!isHello && "Goodbye"}
-    </span>
-  );
-}
+
+// function Hello(){
+//   var isHello = false;
+//   return(
+//    <span>
+//     {isHello && "Hello"}
+//     {!isHello && "Goodbye"}
+//     </span>
+//   );
+// }
 
 // function World(){
 //   return(
@@ -25,4 +24,16 @@ function Hello(){
 //   )
 // }
 
-ReactDom.render(<HelloWorld/>, document.querySelector('#root'));
+
+// function P() {
+//     const fName = 'Eirik';
+//     const lName = 'T';
+//     return (
+//         <Person age = {26}
+//             name = {fName + " " + lName}/>
+//     );
+// }
+
+// const Person = props => <h1>{props.name+' '+props.age}</h1>
+
+ReactDom.render(<HelloWorld name="Eirik"/>,document.querySelector('#root'));
