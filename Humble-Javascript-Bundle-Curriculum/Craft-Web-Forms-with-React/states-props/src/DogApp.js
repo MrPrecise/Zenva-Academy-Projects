811704a1b4b67c11b5c29e433a6abc23adec14f7
Eirik <MrPrecise@users.noreply.github.com>
2022-09-26 18:26:41 +0200

    Lesson 4
    DataFlow 2

--- Humble-Javascript-Bundle-Curriculum/Craft-Web-Forms-with-React/states-props/src/DogApp.js
+++ Humble-Javascript-Bundle-Curriculum/Craft-Web-Forms-with-React/states-props/src/DogApp.js
@@ -1,13 +1,58 @@
 import React from "react";
 
 class DogApp  extends React.Component {
+    state = {
+        newDog: '',
+        dogs: []
+    }
+    handleChange(e) {
+        this.setState({
+            newDog: e.target.value
+        });
+    }
+
+    handleAddDogClick() {
+        if (!this.state.newDog) {
+            return;
+        }
+        this.setState({
+            newDog: '',
+            dogs: [...this.state.dogs, this.state.newDog]
+        });
+    }
+
     render() {
         return (
-           <div>
-            <h1>My Dog</h1>
-            <button>Add a dog</button>
-           </div>
+            <div>
+                <h1>My Dogs</h1>
+                <input 
+                type="text"
+                placeholder="Enter Dog Name"
+                onChange={this.handleChange.bind(this)} 
+                name="newDog" />
+                <ul> 
+                    {this.state.dogs.map((dogs, i) => (
+                       <Dog key={`${dogs}_${i}`} name={dogs}/> 
+                        // Old code before making Dog into an own class
+                        // <li key={`${dogs}_${i}`}>
+                        //     <b>{dogs}</b>
+                        // </li>
+                    ))}
+                </ul>
+                <button onClick={this.handleAddDogClick.bind(this)}>
+                  Add a dog
+                </button>
+            </div>
         );
+    }
+}
+
+class Dog extends React.Component {
+    render() {
+        return (
+            <li>
+                <b>{this.props.name}</b>
+            </li>)
     }
 }
 
