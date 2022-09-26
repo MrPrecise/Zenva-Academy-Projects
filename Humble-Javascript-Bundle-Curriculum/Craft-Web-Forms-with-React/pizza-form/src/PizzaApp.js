75b1f160391e78265db39be011c69fb29dbf54e1
Eirik <MrPrecise@users.noreply.github.com>
2022-09-26 19:28:13 +0200

    Lesson 8
    Form Validation - Part 1

--- Humble-Javascript-Bundle-Curriculum/Craft-Web-Forms-with-React/pizza-form/src/PizzaApp.js
+++ Humble-Javascript-Bundle-Curriculum/Craft-Web-Forms-with-React/pizza-form/src/PizzaApp.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './index.css';
-
 class PizzaApp extends React.Component {
     state = {
         size: 'medium',
         glutenFree: false,
         topping: '',
-        instructions: ''
+        instructions: '',
+        pizzaName: null,
+        email: null,
+        password: null,
+        errors: {
+            pizzaName: '',
+            email: '',
+            password: '',
+        }
     };
-
     setSize = event => {
         this.setState({
             size: event.target.value
+        });
+    };
+    setGlutenFree = event => {
+        this.setState({
+            glutenFree: event.target.checked
         });
     };
     setInstructions = event => {
@@ -24,12 +35,6 @@ class PizzaApp extends React.Component {
             topping: event.target.value
         });
     };
-    setGlutenFree = event => {
-        this.setState({
-            glutenFree: event.target.checked
-        });
-    };
-
     handleSubmit = event => {
         event.preventDefault();
         const {
@@ -39,13 +44,11 @@ class PizzaApp extends React.Component {
             instructions
         } = this.state;
         alert(`Your order:
-        Size: ${ size }
+        Size: ${size}
         Gluten free ? ${glutenFree ? 'yes' : 'no'}
         Topping: ${topping || 'none'}
         Special instructions: ${instructions || 'none'}`);
-    }
-
-
+    };
     render() {
         const {
             size,
@@ -53,76 +56,100 @@ class PizzaApp extends React.Component {
             instructions,
             topping
         } = this.state;
-
-
         return (
-            <>
-            <h1>Order Your Pizza</h1>
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    <input
-                        type="radio"
-                        value="small"
-                        checked={size === 'small'}
-                        onChange={this.setSize}
-                    />
-                    Small
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        value="medium"
-                        checked={size === 'medium'}
-                        onChange={this.setSize}
-                    />
-                    Medium
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        value="large"
-                        checked={size === 'large'}
-                        onChange={this.setSize}
-                    />
-                    Large
-                </label>
-                <br />
-                <br />
-                <label>
-                    Topping
-                    <br />
-                    <select onChange={this.setTopping} value={topping}>
-                    <option value="">- Pick a topping -</option>
-                    <option value="pepperoni">Pepperoni</option>
-                    <option value="peppers+onions">Peppers and onions</option>
-                    <option value="pineapple">Pineapple</option>
-                </select>
-                </label>
-                <br/>
-                <br/>
-                <label>
-                    <input type="checkbox"
-                    checked={glutenFree}
-                    onChange={this.setGlutenFree}/>
-                    Gluten Free
-                </label>
-                <br />
-                <br />
-                <label>
-                    Special instructions:
-                    <br/>
-                    <textarea
-                    value={instructions}
-                    onChange={this.setInstructions}
-                    />
-                </label>
-                <br />
-                <br />
-                <button type="submit"> Send Order </button>
-            </form>
-            </>
+            <div className="wrapper">
+                <div className="form-wrapper">
+                    <h1>Order Your Pizza</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="pizzaName">
+                            <label>Pizza Name</label>
+                            <input type="text"
+                                name="pizzaName"
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="email">
+                            <label>Email</label>
+                            <input type="email"
+                                name="email"
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="password">
+                            <label>Password</label>
+                            <input type="password"
+                                name="password"
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <label>
+                            <input
+                                type="radio"
+                                value="small"
+                                checked={size === 'small'}
+                                onChange={this.setSize}
+                            />
+                            Small
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                value="medium"
+                                checked={size === 'medium'}
+                                onChange={this.setSize}
+                            />
+                            Medium
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                value="large"
+                                checked={size === 'large'}
+                                onChange={this.setSize}
+                            />
+                            Large
+                        </label>
+                        <br />
+                        <br />
+                        <div className="topping">
+                            <label>
+                                Topping
+                                <br />
+                                <select onChange={this.setTopping} value={topping}>
+                                    <option value="">- Pick a topping -</option>
+                                    <option value="pepperoni">Pepperoni</option>
+                                    <option value="peppers+onions">Peppers and onions</option>
+                                    <option value="pineapple">Pineapple</option>
+                                </select>
+                            </label>
+                        </div>
+                        <br />
+                        <br />
+                        <label>
+                            <input type="checkbox"
+                                checked={glutenFree}
+                                onChange={this.setGlutenFree} />
+                            GlutenFree
+                        </label>
+                        <br />
+                        <br />
+                        <div className="instructions">
+                            <label>
+                                Special instructions:
+                                <br />
+                                <textarea
+                                    value={instructions}
+                                    onChange={this.setInstructions}
+                                />
+                            </label>
+                        </div>
+                        <br />
+                        <br />
+                        <button type="submit"> Send Order </button>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
-
 export default PizzaApp;
