61ad900645875d43af94b14b6f13b07083d6101e
Eirik <MrPrecise@users.noreply.github.com>
2022-09-26 17:59:38 +0200

    Lesson 3 + Init
    DataFlow Part 1

---
+++ Humble-Javascript-Bundle-Curriculum/Craft-Web-Forms-with-React/states-props/src/DogApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class DogApp  extends React.Component {
+    render() {
+        return (
+           <div>
+            <h1>My Dog</h1>
+            <button>Add a dog</button>
+           </div>
+        );
+    }
+}
+
+export default DogApp;
---
+++ Humble-Javascript-Bundle-Curriculum/Craft-Web-Forms-with-React/states-props/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DogApp from './DogApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DogApp />
+  </React.StrictMode>
+);
