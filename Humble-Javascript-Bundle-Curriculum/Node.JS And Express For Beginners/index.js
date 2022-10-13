a61f584049d7ee049ab7701b2c2bc251f8f1b7c5
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-10-13 14:37:40 +0200

    Lesson 5
    Express Routing

--- Humble-Javascript-Bundle-Curriculum/Node.JS And Express For Beginners/index.js
+++ Humble-Javascript-Bundle-Curriculum/Node.JS And Express For Beginners/index.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/',  (request, response) => {
+    console.log(request);
+    response.send('Main Page');
+});
+
+app.get('/1',  (request, response) => {
+    console.log(request);
+    response.send('Page One');
+});
+
+app.get('/2',  (request, response) => {
+    response.send('Page 2');
+});
+
+app.get('/3',  (request, response) => {
+    response.send('3');
+});
+
 app.listen(port, ()=>{
     console.log(`This is port ${port}`)
 })
