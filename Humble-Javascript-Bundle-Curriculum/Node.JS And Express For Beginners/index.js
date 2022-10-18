ed7975229b739a8141c61d5c5e1c97375d595ae6
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-10-18 10:20:27 +0200

    Lesson 14
    Dotenv install

--- Humble-Javascript-Bundle-Curriculum/Node.JS And Express For Beginners/index.js
+++ Humble-Javascript-Bundle-Curriculum/Node.JS And Express For Beginners/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routes = require('./routes/main')
 const passwordRoutes = require('./routes/password')
 
