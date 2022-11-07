e73db3f2ea521e40c1b9aef6ac4b17c7f13b1252
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-11-07 15:30:40 +0100

    Update TestScene.js

--- Humble-Javascript-Bundle-Curriculum/Build an RPG Adventure in Phaser/assets/js/scenes/TestScene.js
+++ Humble-Javascript-Bundle-Curriculum/Build an RPG Adventure in Phaser/assets/js/scenes/TestScene.js
@@ -1,25 +1,18 @@
 class TestScene extends Phaser.Scene {
   constructor() {
-    super('Test');
+    super("Test");
   }
 
   create() {
-      this.physics.world.bounds.width = 1000;
+    this.physics.world.bounds.width = 1000;
     this.physics.world.bounds.height = 1000;
-      
-    this.monster = new Monster(this,
-        900,
-        900,
-        'monsters',
-        0,
-        0,
-        10,
-        10);
-      this.physics.moveTo(this.monster, 2000, 2000);
+
+    this.monster = new Monster(this, 900, 900, "monsters", 0, 0, 10, 10);
+    this.physics.moveTo(this.monster, 2000, 2000);
   }
-    
-    update() {
-        console.log(this.monster.x);
-        console.log(this.monster.y);
-    }
+
+  update() {
+    console.log(this.monster.x);
+    console.log(this.monster.y);
+  }
 }
