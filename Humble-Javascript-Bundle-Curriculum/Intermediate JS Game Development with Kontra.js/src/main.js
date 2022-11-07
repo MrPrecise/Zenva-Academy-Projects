37d37121297ee4fc95224ae9ebdc8c6f1b0e8c02
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-11-07 20:03:51 +0100

    Lesson 6

--- Humble-Javascript-Bundle-Curriculum/Intermediate JS Game Development with Kontra.js/src/main.js
+++ Humble-Javascript-Bundle-Curriculum/Intermediate JS Game Development with Kontra.js/src/main.js
@@ -1,15 +1,29 @@
-const { init, GameLoop } = kontra;
+const { init, GameLoop, Sprite } = kontra;
 
 const { canvas, context } = init();
 
-console.log(canvas, context);
+const sprite = Sprite({
+  x: 200,
+  y: 200,
+  color: "blue",
+  width: 50,
+  height: 20,
+  dx: 2,
+});
 
 const loop = GameLoop({
   update: () => {
-    console.log("Update");
+    // console.log("Update");
+    sprite.update();
+    if (sprite.x > canvas.width) {
+      sprite.x = -sprite.width;
+    }
   },
   render: () => {
-    console.log("Render");
+    // console.log("Render");
+    sprite.render();
   },
 });
 loop.start();
+
+console.log(canvas, context, sprite);
