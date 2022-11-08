33fca35b2bde8e1d8f5de8580eba201433754a29
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-11-08 19:53:06 +0100

    Lesson 26
    Loading in assets

--- Humble-Javascript-Bundle-Curriculum/Intermediate JS Game Development with Kontra.js/src/Game.js
+++ Humble-Javascript-Bundle-Curriculum/Intermediate JS Game Development with Kontra.js/src/Game.js
@@ -1,6 +1,6 @@
 import Grid from "./Grid.js";
 import Board from "./Board.js";
-const { init, GameLoop, Sprite, initPointer, track } = kontra;
+const { init, GameLoop, Sprite, initPointer, track, load, on } = kontra;
 
 export default class Game {
   constructor(width, height) {
@@ -31,10 +31,35 @@ export default class Game {
     this.grid.render();
   }
   update() {}
+
   load() {
     console.log("Loading Assets");
-    this.start();
+
+    on("assetLoaded", (asset, url) => {
+      asset.id = url;
+    });
+
+    load(
+      "assets/images/bean_blue.png",
+      "assets/images/bean_dead.png",
+      "assets/images/bean_green.png",
+      "assets/images/bean_orange.png",
+      "assets/images/bean_pink.png",
+      "assets/images/bean_purple.png",
+      "assets/images/bean_red.png",
+      "assets/images/bean_white.png",
+      "assets/images/bean_yellow.png"
+    )
+      .then((assets) => {
+        this.assets = assets;
+        this.start();
+        console.log(this.assets);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
+
   start() {
     console.log("Starting Game");
     this.gameLoop.start();
