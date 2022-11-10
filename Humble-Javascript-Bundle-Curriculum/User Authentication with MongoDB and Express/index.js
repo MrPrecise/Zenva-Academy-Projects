9f15a269671f2bcd41cb2789cceb1a86129726d0
Eirik Thue <MrPrecise@users.noreply.github.com>
2022-11-10 18:49:48 +0100

    Project init

---
+++ Humble-Javascript-Bundle-Curriculum/User Authentication with MongoDB and Express/index.js
@@ -0,0 +1,83 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+/**
+ * Setup Mongo Connection
+ */
+const uri = process.env.MONGO_CONNECTION_URL;
+const mongoConfig = {
+  useNewUrlParser: true,
+};
+
+/**
+ * Connect to MongoDB with config from Setup
+ */
+mongoose.connect(uri, mongoConfig);
+
+/**
+ * On connection
+ */
+mongoose.connection.on("error", (error) => {
+  console.log("Error in the process:");
+  console.log(error);
+  process.exit(1);
+});
+
+/**
+ * On succesful connection
+ */
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+/**
+ *
+ */
+const app = express();
+const port = process.env.PORT || 3000;
+const routes = require("./routes/main");
+const passwordRoutes = require("./routes/password");
+
+/**
+ * Update express settings
+ */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN }));
+
+/**
+ * Require Passort Auth
+ */
+require("./auth/auth");
+
+/**
+ * Setup routes
+ */
+app.use("/", routes);
+app.use("/", passwordRoutes);
+
+/**
+ * Catch not found Routes
+ */
+app.use((request, response) => {
+  response.status(404).json({ message: "404 - Not Found", status: 404 });
+});
+
+/**
+ * Handle errors
+ */
+app.use((error, request, response, next) => {
+  console.log(error);
+  response
+    .status(error.status || 500)
+    .json({ error: error.message, status: 500 });
+});
+
+app.listen(port, () => {
+  console.log(`This is port ${port}`);
+});
