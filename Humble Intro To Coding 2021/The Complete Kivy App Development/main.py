9c4cd471cea04ea9f8a2a389f5c16e694ab53c57
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-03-23 17:59:18 +0100

    Adding images to the build

--- Humble Intro To Coding 2021/The Complete Kivy App Development/main.py
+++ Humble Intro To Coding 2021/The Complete Kivy App Development/main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from kivy.uix.image import Image
 
 
 class GameScreen(Widget):
@@ -10,9 +11,6 @@ class GameScreen(Widget):
 class LanguageApp(App):
     def build(self):
         game_screen = GameScreen()
-        game_screen.add_widget(
-            Label(text='Hello World')
-        )
         return game_screen
     
 if __name__ == '__main__':
