a1667724ff3effded4ca7050e273ab271677dd1b
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-03-23 16:58:03 +0100

    Used Kivy for Multiple Widgets
    Used Kivy for Multiple Widgets

--- Humble Intro To Coding 2021/The Complete Kivy App Development/main.py
+++ Humble Intro To Coding 2021/The Complete Kivy App Development/main.py
@@ -3,16 +3,16 @@ from kivy.uix.button import Button
 
 
 class CoolButton(Button):
-    def __init__(self, **kwargs):
-        super(CoolButton, self).__init__(**kwargs)
-        self.text="Cool Button"
-        self.pos=(100,100)
-        self.size_hint=(.25,.25)
+    pass
 
 
 class LanguageApp(App):
     def build(self):
-        return CoolButton()
+        return CoolButton(
+            pos=(100,100),
+            size_hint=(None, None),
+            size=(200,200)
+        )
     
 if __name__ == '__main__':
     LanguageApp().run()
