0a8134e49d310ee6c3337547e0e5b82a1cbb19c4
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-03-23 16:31:51 +0100

    Resizing apps
    Added position and resizing to button

--- Humble Intro To Coding 2021/The Complete Kivy App Development/main.py
+++ Humble Intro To Coding 2021/The Complete Kivy App Development/main.py
@@ -3,7 +3,11 @@ from kivy.uix.button import Button
 
 class LanguageApp(App):
     def build(self):
-        return Button(text='Click Me')
+        return Button(
+            text='Click Me',
+            pos=(50,50),
+            size=(100,100),
+            size_hint=(0.2,0.2))
     
 if __name__ == '__main__':
     LanguageApp().run()
