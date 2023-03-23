702674d02bf6bb93f1a4b9b9410e19c226abf996
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-03-23 18:11:35 +0100

    Added functionality to parse to console
    Parsing text to console

--- Humble Intro To Coding 2021/The Complete Kivy App Development/main.py
+++ Humble Intro To Coding 2021/The Complete Kivy App Development/main.py
@@ -3,10 +3,12 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.image import Image
-
+from kivy.properties import StringProperty
+from kivy.properties import NumericProperty
 
 class GameScreen(Widget):
-    pass
+  def press_button(self):
+    print("Button pressed!")
 
 class LanguageApp(App):
     def build(self):
