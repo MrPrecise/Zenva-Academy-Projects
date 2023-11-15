f62da1c7613bb1ec44737d3d01a0da1cae63d1b3
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-11-15 17:27:45 +0100

    App Structure – Part 1
    App Structure – Part 1

--- Humble-Intro-To-Coding-2023/JavaScript-Projects-Language-Game/script.js
+++ Humble-Intro-To-Coding-2023/JavaScript-Projects-Language-Game/script.js
@@ -5,6 +5,22 @@ let question = {
   correctAnswer: 1,
 };
 
+// function for starting the application
+function start() {
+  // get alternatives
+  let alts = document.querySelectorAll(".alternative");
+
+  alts.forEach(function (element, index) {
+    element.addEventListener("click", function () {
+      // check correct answer
+      console.log("check correct answer");
+    });
+  });
+
+  // show first question
+  showQuestion(question);
+}
+
 // function for showing the question
 function showQuestion(q) {
   // show question title
@@ -16,26 +32,8 @@ function showQuestion(q) {
 
   alts.forEach(function (element, index) {
     element.textContent = q.alternatives[index];
-
-    // listening for events
-    element.addEventListener("click", function () {
-      // check correct answer
-      if (q.correctAnswer == index) {
-        console.log("Correct Answer!");
-      } else {
-        console.log("Wrong Answer!");
-      }
-    });
   });
 }
 
-// call the function
-showQuestion(question);
-
-// select the button element
-let btn = document.getElementById("btn");
-
-// listening to click events
-btn.addEventListener("click", function () {
-  console.log("Clicked!");
-});
+// call the function for starting the application
+start();
