891c3386ef80f6df4f45f3d5f8c657fc696fe944
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-11-15 17:39:49 +0100

    App Structure – Part 2
    App Structure – Part 2

--- Humble-Intro-To-Coding-2023/JavaScript-Projects-Language-Game/script.js
+++ Humble-Intro-To-Coding-2023/JavaScript-Projects-Language-Game/script.js
@@ -5,35 +5,35 @@ let question = {
   correctAnswer: 1,
 };
 
-// function for starting the application
-function start() {
-  // get alternatives
-  let alts = document.querySelectorAll(".alternative");
+let app = {
+  start: function () {
+    // get alternatives
+    let alts = document.querySelectorAll(".alternative");
 
-  alts.forEach(function (element, index) {
-    element.addEventListener("click", function () {
-      // check correct answer
-      console.log("check correct answer");
+    alts.forEach(function (element, index) {
+      element.addEventListener("click", function () {
+        // check correct answer
+        console.log("check correct answer");
+      });
     });
-  });
 
-  // show first question
-  showQuestion(question);
-}
+    // show first question
+    this.showQuestion(question);
+  },
 
-// function for showing the question
-function showQuestion(q) {
-  // show question title
-  let titleDiv = document.getElementById("title");
-  titleDiv.textContent = q.title;
+  showQuestion: function (q) {
+    // show question title
+    let titleDiv = document.getElementById("title");
+    titleDiv.textContent = q.title;
 
-  // show alternatives
-  let alts = document.querySelectorAll(".alternative");
+    // show alternatives
+    let alts = document.querySelectorAll(".alternative");
 
-  alts.forEach(function (element, index) {
-    element.textContent = q.alternatives[index];
-  });
-}
+    alts.forEach(function (element, index) {
+      element.textContent = q.alternatives[index];
+    });
+  },
+};
 
-// call the function for starting the application
-start();
+// initialize the application
+app.start();
