51582b8d1d408fab129e6117b42cb302281f20ac
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-11-16 20:34:33 +0100

    Displaying Cards
    Displaying Cards

--- Humble-Intro-To-Coding-2023/Build-a-Dynamic-Data-Table/script.js
+++ Humble-Intro-To-Coding-2023/Build-a-Dynamic-Data-Table/script.js
@@ -4,7 +4,32 @@ async function loadCards() {
       "http://127.0.0.1:5500/Humble-Intro-To-Coding-2023/Build-a-Dynamic-Data-Table/data.json"
     );
     let cardsJson = await response.json();
-    console.log(cardsJson);
+    let cardTable = document.getElementById("myCards");
+
+    var cards = cardsJson.map(function (card) {
+      var cardsToShow = {
+        id: card.id,
+        name: card.name,
+        price: card.price,
+        isRare: card.isRare,
+      };
+      return cardsToShow;
+    });
+
+    cardTable.innerHTML =
+      "<tr><td>ID</td><td>Name</td><td>Price</td><td>Is Rare</td>";
+    for (var i = 0; i < cards.length; i++) {
+      cardTable.innerHTML +=
+        "<tr><td>" +
+        cards[i].id +
+        "</td><td>" +
+        cards[i].name +
+        "</td><td>" +
+        cards[i].price +
+        "</td><td>" +
+        cards[i].isRare +
+        "</td></tr>";
+    }
   } catch (err) {
     alert(err);
   }
