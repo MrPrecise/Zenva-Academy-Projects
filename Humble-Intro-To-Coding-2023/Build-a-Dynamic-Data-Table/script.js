601f3243b85a016dea61e4d2504ee0aec04bf654
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-11-16 20:52:46 +0100

    Displaying Rare Cards
    Displaying Rare Cards

--- Humble-Intro-To-Coding-2023/Build-a-Dynamic-Data-Table/script.js
+++ Humble-Intro-To-Coding-2023/Build-a-Dynamic-Data-Table/script.js
@@ -5,6 +5,7 @@ async function loadCards() {
     );
     let cardsJson = await response.json();
     let cardTable = document.getElementById("myCards");
+    let onlyRares = document.getElementById("raresOnly");
 
     var cards = cardsJson.map(function (card) {
       var cardsToShow = {
@@ -15,6 +16,43 @@ async function loadCards() {
       };
       return cardsToShow;
     });
+
+    let rareCards = cards
+      .filter((obj) => obj.isRare == true)
+      .map((obj) => ({
+        id: obj.id,
+        name: obj.name,
+        priceUSD: obj.priceUSD,
+        isRare: obj.isRare,
+      }));
+
+    if (onlyRares.checked) {
+      for (var i = 0; i < rareCards.length; i++) {
+        cardTable.innerHTML +=
+          "" +
+          rareCards[i].id +
+          "" +
+          rareCards[i].name +
+          "" +
+          rareCards[i].price +
+          "" +
+          rareCards[i].isRare +
+          "";
+      }
+    } else {
+      for (var i = 0; i < cards.length; i++) {
+        cardTable.innerHTML +=
+          "" +
+          cards[i].id +
+          "" +
+          cards[i].name +
+          "" +
+          cards[i].price +
+          "" +
+          cards[i].isRare +
+          "";
+      }
+    }
 
     cardTable.innerHTML =
       "<tr><td>ID</td><td>Name</td><td>Price</td><td>Is Rare</td>";
