46f5066bf8af9c9f2556e8f65d3f8f452e6405b2
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-11-27 10:19:58 +0100

    Update game.js
    Rendering Sprites

--- Humble-Intro-To-Coding-2023/Create-a-Road-Crossing-Game/js/game.js
+++ Humble-Intro-To-Coding-2023/Create-a-Road-Crossing-Game/js/game.js
@@ -1,5 +1,20 @@
 // Create new scene
 let game_scene = new Phaser.Scene("Game");
+
+// Load Assets
+game_scene.preload = function () {
+  this.load.image("background", "assets/background.png");
+  this.load.image("player", "assets/player.png");
+};
+
+// Called once after preload ends
+game_scene.create = function () {
+  let bg = this.add.sprite(0, 0, "background");
+  bg.setPosition(640 / 2, 360 / 2);
+  let game_width = this.sys.game.config.width;
+  let game_height = this.sys.game.config.height;
+};
+
 // Set configuration of the game
 let config = {
   type: Phaser.AUTO,
