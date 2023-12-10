bba6051f70765eaf2be0bb13eb14cd18a518c83a
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-12-10 17:56:27 +0100

    Update main.py

--- Humble-Intro-To-Coding-2023/Python-Projects-Object-Oriented-Game/main.py
+++ Humble-Intro-To-Coding-2023/Python-Projects-Object-Oriented-Game/main.py
@@ -1,6 +1,7 @@
 class GameObject:
-    def __init__(self, name, apperence, feel, smell):
+    def __init__(self, name, apperence, feel, smell, c):
         self.name = name
         self.apperence = apperence
         self.feel = feel
         self.smell = smell
+        self.c = c
