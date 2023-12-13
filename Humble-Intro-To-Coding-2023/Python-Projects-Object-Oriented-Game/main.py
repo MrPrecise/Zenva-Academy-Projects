e9237b67cdfc97952fe5b462bc0d9b4127ebb1ac
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-12-13 07:44:16 +0100

    Update main.py
    Updated class with look, touch, sniff functions

--- Humble-Intro-To-Coding-2023/Python-Projects-Object-Oriented-Game/main.py
+++ Humble-Intro-To-Coding-2023/Python-Projects-Object-Oriented-Game/main.py
@@ -1,7 +1,27 @@
 class GameObject:
-    def __init__(self, name, apperence, feel, smell, c):
+    def __init__(self, name, apperence, feel, smell):
         self.name = name
         self.apperence = apperence
         self.feel = feel
         self.smell = smell
-        self.c = c
+
+    # Implementing the methods of our class
+    def look(self):
+        # We're formatting the string so it shows the name and appearance of the object
+        return f"You look at the {self.name}. {self.appearance}\n"
+
+    def touch(self):
+        return f"You touch the {self.name}. {self.feel}\n"
+
+    def sniff(self):
+        return f"You sniff the {self.name}. {self.smell}\n"
+
+
+# Creating a game object that instantiates our GameObject class
+game_object = GameObject("Knife", "Some appearance", "Some feel", "Some smell")
+
+# We can access an object's fields to modify them
+game_object.name = "Spoon"
+
+# And call out for its methods as seen below
+print(game_object.sniff())
