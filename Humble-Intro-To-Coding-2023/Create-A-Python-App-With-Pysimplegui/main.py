cd19a74c8ac66dc0915a1d2950c08b5903447ab9
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-12-28 18:04:41 +0100

    Project Implementation
    Project Implementation

---
+++ Humble-Intro-To-Coding-2023/Create-A-Python-App-With-Pysimplegui/main.py
@@ -0,0 +1,9 @@
+from patient import Patient
+from datetime import datetime
+
+patients = [
+    Patient("1 User", "Test", datetime(1990, 5, 6), 180, 85.3, True),
+    Patient("2 User", "Test", datetime(1991, 4, 3), 169, 75, True),
+    Patient("3 User", "Test", datetime(1992, 1, 2), 175, 80.3, True)]
+
+print(patients[0].first_name)
---
+++ Humble-Intro-To-Coding-2023/Create-A-Python-App-With-Pysimplegui/patient.py
@@ -0,0 +1,15 @@
+class Patient:
+    def __init__(self,
+                 first_name,
+                 last_name,
+                 date_of_birth,
+                 height,
+                 weight,
+                 is_taking_medication):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.height = height
+        self.weight = weight
+        self.is_taking_medication = is_taking_medication
