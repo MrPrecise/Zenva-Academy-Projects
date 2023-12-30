9d13a2ba3e43bfc5016b60bac395ad0efe62caa9
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-12-30 20:20:29 +0100

    Added import and function
    Added import for PySimpleGUI
    Added Function for making table from our patient info

--- Humble-Intro-To-Coding-2023/Create-A-Python-App-With-Pysimplegui/main.py
+++ Humble-Intro-To-Coding-2023/Create-A-Python-App-With-Pysimplegui/main.py
@@ -1,9 +1,20 @@
 from patient import Patient
 from datetime import datetime
+import PySimpleGUI as sg
 
 patients = [
     Patient("1 User", "Test", datetime(1990, 5, 6), 180, 85.3, True),
     Patient("2 User", "Test", datetime(1991, 4, 3), 169, 75, True),
     Patient("3 User", "Test", datetime(1992, 1, 2), 175, 80.3, True)]
 
-print(patients[0].convert_value_to_strings())
+
+# Returns a list with all patients' info
+def convert_patients_to_table_data():
+    patients_data = []
+    for patient in patients:
+        strings = patient.convert_value_to_strings()
+        patients_data.append(strings)
+    return patients_data
+
+
+print(convert_patients_to_table_data())
