74373c38e19c819e00692be0839f77f2fd497b93
Eirik Thue <MrPrecise@users.noreply.github.com>
2024-01-05 04:30:18 +0100

    Update main.py
    Updated main with button to add patient
    Added event to react to the button click

--- Humble-Intro-To-Coding-2023/Create-A-Python-App-With-Pysimplegui/main.py
+++ Humble-Intro-To-Coding-2023/Create-A-Python-App-With-Pysimplegui/main.py
@@ -5,21 +5,31 @@ import data_function
 table_headings = [
     "First name", "Last name", "Date of birth", "Height", "Weight", "Is taking medication?"
 ]
-
 # Patients table rows data
 table_data = data_function.convert_patients_to_table_data()
 
+# Handler function for the 'Add new patient' button
+
+
+def press_add_patient_button():
+    print("Button pressed")
+
+
 # Patients table layout
 patients_window_layout = [
+    [sg.Text("All Patient Data"), sg.Button("Add new patient")],
     [sg.Table(headings=table_headings, values=table_data)]
 ]
 
 # Displays our table
-patients_window = sg.Window('Patients List', patients_window_layout)
+patients_window = sg.Window("Patients List", patients_window_layout)
 
+# Listening for events
 while True:
     event, values = patients_window.read()
     if event == sg.WIN_CLOSED:
         break
+    elif event == "Add new patient":
+        press_add_patient_button()
 # Closes the window
 patients_window.close()
