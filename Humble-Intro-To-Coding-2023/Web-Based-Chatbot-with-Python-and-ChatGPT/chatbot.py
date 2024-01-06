0c2f42234a2a3e9b0bb0ec881611b510c3e7ee79
Eirik Thue <MrPrecise@users.noreply.github.com>
2024-01-06 04:20:46 +0100

    Update chatbot.py
    Updated a system initiator
    And messages can carry on after initial message

--- Humble-Intro-To-Coding-2023/Web-Based-Chatbot-with-Python-and-ChatGPT/chatbot.py
+++ Humble-Intro-To-Coding-2023/Web-Based-Chatbot-with-Python-and-ChatGPT/chatbot.py
@@ -6,21 +6,26 @@ client = OpenAI(
     api_key=os.environ.get('OPENAI_API_KEY')
 )
 
-# Array with messages
-messages = []
+messages = [{
+    'role': 'system',
+    'content': 'You are a quiz. Present the user with a multiple-choice question to practice for a python interview, they have to respond by typing a, b, c, d or e. Only one question at a time. Wait until the user responds before presenting a new question or the answer to the previous question'
+}]
 
 while True:
-    # capture user input
-    user_input = input('Enter your prompt: ')
-    # quit loop if user presses "q"
-    if user_input == 'q':
-        exit()
-    # prompt preparation
-    messages.append({'role': 'user', 'content': user_input})
     # send the api call
     response = client.chat.completions.create(
         messages=messages, model="gpt-3.5-turbo")
     # display response in console
     print(response.choices[0].message.content)
     # expanding the conversation
-    messages.append(response.choices[0])
+    messages.append(response.choices[0].message)
+
+    # adding prompt again to continue with the quiz
+    messages.append(prompt)
+    # capture user input
+    user_input = input('Enter your answer: ')
+    # quit loop if user presses "q"
+    if user_input == 'q':
+        exit()
+    # prompt preparation
+    messages.append({'role': 'user', 'content': user_input})
