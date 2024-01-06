ac742d5b6370fe012a023d65523e06fcdd9db8b7
Eirik Thue <MrPrecise@users.noreply.github.com>
2024-01-06 02:19:04 +0100

    Update bot.py
    Added comments on code

--- Humble-Intro-To-Coding-2023/Create-a-Bot-with-Python-and-ChatGPT/bot.py
+++ Humble-Intro-To-Coding-2023/Create-a-Bot-with-Python-and-ChatGPT/bot.py
@@ -1,17 +1,30 @@
 from openai import OpenAI
 
+# Create OpenAI Client with API Key
 client = OpenAI(
     api_key=""
 )
 
+# Added messages
 message = {'role': 'user',
            'content': 'What is the square root of four houndred?'
            }
+
+# Added array that can be filled with messages later on
 messages = []
+
+# Append prior message
 messages.append(message)
 
+# Used ChatGPT 3.5 that was used in the course
+# However there are many options we can find under here:
+# https://platform.openai.com/account/limits
 response = client.chat.completions.create(
     messages=messages, model="gpt-3.5-turbo"
 )
 
+# Print Full Response
 print(response)
+
+# Print Actual Message
+print(response.choices[0].message.content)
