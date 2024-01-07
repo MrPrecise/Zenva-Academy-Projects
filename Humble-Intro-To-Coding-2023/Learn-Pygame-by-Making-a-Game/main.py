1df32cac40b5a5f1fbf3369d8b12a8670d2c0391
Eirik Thue <MrPrecise@users.noreply.github.com>
2024-01-07 06:01:33 +0100

    Update main.py

--- Humble-Intro-To-Coding-2023/Learn-Pygame-by-Making-a-Game/main.py
+++ Humble-Intro-To-Coding-2023/Learn-Pygame-by-Making-a-Game/main.py
@@ -9,31 +9,39 @@ pygame.init()
 game_clock = pygame.time.Clock()
 
 
+# Game code goes here!
+# For example: a game loop that prints a message to the console
+def run_game_loop():
+    while True:
+        # Handle events
+        events = pygame.event.get()
+
+        # Loops for all occurring events
+        for event in events:
+            # Checks if any of them is the QUIT event
+            if event.type == pygame.QUIT:
+                print("Quit")
+                # Breaks the game loop by exiting the whole function
+                exitgame()
+
+        # Values for gamewindow width / height
+        width = 800
+        height = 800
+
+        # Creating game windod with width / height variable
+        game_window = pygame.display.set_mode((width, height))
+
+        white_colour = (255, 255, 255)
+        game_window.fill(white_colour)
+
+        pygame.display.update()
+        game_clock.tick(60)
+
+
 def exitgame():
     # Closing Pygame and our application
     pygame.quit()
     quit()
 
 
-# Game code goes here!
-# For example: a game loop that prints a message to the console
-while True:
-    print("Pygame is running!")
-
-    # Values for gamewindow width / height
-    width = 800
-    height = 800
-
-    # Creating game windod with width / height variable
-    game_window = pygame.display.set_mode((width, height))
-
-    white_colour = (255, 255, 255)
-    game_window.fill(white_colour)
-
-    pygame.display.update()
-    game_clock(60)
-
-    pygame.time.delay(5000)
-    if (True):
-        print("Exiting")
-        exitgame()
+run_game_loop()
