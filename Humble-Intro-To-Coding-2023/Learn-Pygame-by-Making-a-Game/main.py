dbbdcbd61c3d1c94e6124fe2fc428f5b3f2b33f5
Eirik Thue <MrPrecise@users.noreply.github.com>
2024-01-07 04:00:09 +0100

    Update main.py
    Creating the Game Window
    Added the game window to expire after 5 seconds

--- Humble-Intro-To-Coding-2023/Learn-Pygame-by-Making-a-Game/main.py
+++ Humble-Intro-To-Coding-2023/Learn-Pygame-by-Making-a-Game/main.py
@@ -4,12 +4,31 @@ import pygame
 # initializing the Pygame library
 pygame.init()
 
+
+def exitgame():
+    # Closing Pygame and our application
+    pygame.quit()
+    quit()
+
+
 # Game code goes here!
 # For example: a game loop that prints a message to the console
 while True:
     print("Pygame is running!")
-    pygame.time.delay(5000)
 
-    # Closing Pygame and our application
-    pygame.quit()
-    quit()
+    # Values for gamewindow width / height
+    width = 800
+    height = 800
+
+    # Creating game windod with width / height variable
+    game_window = pygame.display.set_mode((width, height))
+
+    white_colour = (255, 255, 255)
+    game_window.fill(white_colour)
+
+    pygame.display.update()
+
+    pygame.time.delay(5000)
+    if (True):
+        print("Exiting")
+        exitgame()
