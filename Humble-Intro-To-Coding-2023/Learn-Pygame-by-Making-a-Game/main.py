cb21cc20f7b61e1e988e5c0f0fba6f02671b3dbf
Eirik Thue <MrPrecise@users.noreply.github.com>
2024-01-07 03:47:41 +0100

    Create main.py
    Creating main.py with concepts for pygame for the "Project Setup" section of course

---
+++ Humble-Intro-To-Coding-2023/Learn-Pygame-by-Making-a-Game/main.py
@@ -0,0 +1,15 @@
+# importing Pygame to our project
+import pygame
+
+# initializing the Pygame library
+pygame.init()
+
+# Game code goes here!
+# For example: a game loop that prints a message to the console
+while True:
+    print("Pygame is running!")
+    pygame.time.delay(5000)
+
+    # Closing Pygame and our application
+    pygame.quit()
+    quit()
