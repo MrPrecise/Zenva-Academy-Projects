0fc3f8fd129ea0f2bcf33fc650d25aad3e2e5d83
Eirik Thue <MrPrecise@users.noreply.github.com>
2024-01-08 02:19:57 +0100

    Assets Import
    Added assests
    Added treasure assets to game scene
    Added background to game scene

--- Humble-Intro-To-Coding-2023/Learn-Pygame-by-Making-a-Game/main.py
+++ Humble-Intro-To-Coding-2023/Learn-Pygame-by-Making-a-Game/main.py
@@ -4,18 +4,28 @@ import pygame
 # initializing the Pygame library
 pygame.init()
 
+# Values for gamewindow width / height and white RGB color
+width = 800
+height = 800
+white_colour = (255, 255, 255)
 
-# Initilzise Clock
+# Initilzise Game Clock and Window
 game_clock = pygame.time.Clock()
+game_window = pygame.display.set_mode((width, height))
+
+# Added Background Image and Scaling it
+background_image = pygame.image.load("assets/background.png")
+background = pygame.transform.scale(background_image, (width, height))
+
+# Added Treasure Image and Scaling it
+treasure_image = pygame.image.load("assets/treasure.png")
+treasure = pygame.transform.scale(treasure_image, (50, 50))
 
 
-# Game code goes here!
-# For example: a game loop that prints a message to the console
 def run_game_loop():
     while True:
         # Handle events
         events = pygame.event.get()
-
         # Loops for all occurring events
         for event in events:
             # Checks if any of them is the QUIT event
@@ -24,16 +34,11 @@ def run_game_loop():
                 # Breaks the game loop by exiting the whole function
                 exitgame()
 
-        # Values for gamewindow width / height
-        width = 800
-        height = 800
-
         # Creating game windod with width / height variable
-        game_window = pygame.display.set_mode((width, height))
 
-        white_colour = (255, 255, 255)
         game_window.fill(white_colour)
-
+        game_window.blit(background, (0, 0))
+        game_window.blit(treasure, (375, 50))
         pygame.display.update()
         game_clock.tick(60)
 
