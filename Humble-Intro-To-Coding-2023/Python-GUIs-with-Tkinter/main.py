1a5cf19068d0f69c424ed809df3d50a36849888c
Eirik Thue <MrPrecise@users.noreply.github.com>
2024-03-03 12:07:25 +0100

    Update main.py
    Added Button Functionality

--- Humble-Intro-To-Coding-2023/Python-GUIs-with-Tkinter/main.py
+++ Humble-Intro-To-Coding-2023/Python-GUIs-with-Tkinter/main.py
@@ -8,10 +8,31 @@ class MyApp:
         root.geometry("250x100")
         root.maxsize(1000, 800)
 
-        label = Label(root, text="Testing out this")
+        self.label_text = StringVar()
+        self.label_text.set("Enter Full Name")
+        label = Label(root)
         label.pack()
+        label.configure(
+            font=("Courier", 20),
+            textvariable=self.label_text
+        )
 
-        label.configure(text="New label", font=("Courier", 20))
+        self.entry_text = StringVar()
+        entry = Entry(
+            root,
+            textvariable=self.entry_text
+        )
+        entry.pack()
+
+        button = Button(
+            root,
+            text="Confirm",
+            command=self.button_pressed
+        )
+        button.pack()
+
+    def button_pressed(self):
+        self.label_text.set(self.entry_text.get())
 
 
 root = Tk()
