79e5c33a6d9c83162db5c46cf69d5d84197f942e
Eirik Thue <MrPrecise@users.noreply.github.com>
2024-03-03 11:35:13 +0100

    Update main.py
    Added Label

--- Humble-Intro-To-Coding-2023/Python-GUIs-with-Tkinter/main.py
+++ Humble-Intro-To-Coding-2023/Python-GUIs-with-Tkinter/main.py
@@ -5,8 +5,11 @@ class MyApp:
     def __init__(self, root):
 
         root.title("Cool Stuff")
-        root.geometry("500x400")
+        root.geometry("250x100")
         root.maxsize(1000, 800)
+
+        label = Label(root, text="Testing out this")
+        label.pack()
 
 
 root = Tk()
